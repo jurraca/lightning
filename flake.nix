74bc5a2eabace9728e14032eafdfca5c5b0a58c9
jurraca <julienu@pm.me>
2023-08-29 18:56:43 +0200

    flake setup: nix develop runs, but clightning doesnt

--- flake.nix
+++ flake.nix
@@ -1,50 +1,76 @@
-  version = "23.05.2";
+{
+  description = "A very basic flake";
 
-  src = fetchurl {
-    url = "https://github.com/ElementsProject/lightning/releases/download/v${version}/clightning-v${version}.zip";
-    sha256 = "sha256-Tj5ybVaxpk5wmOw85LkeU4pgM9NYl6SnmDG2gyXrTHw=";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # when building on darwin we need dawin.cctools to provide the correct libtool
-  # as libwally-core detects the host as darwin and tries to add the -static
-  # option to libtool, also we have to add the modified gsed package.
-  nativeBuildInputs = [ autoconf autogen automake gettext libtool lowdown protobuf py3 unzip which ]
-    ++ lib.optionals stdenv.isDarwin [ darwin.cctools darwin.autoSignDarwinBinariesHook ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      py3 = pkgs.python3.withPackages (p: [p.mako]);
+    in {
+      packages.default = with pkgs;
+        stdenv.mkDerivation rec {
+          pname = "clightning";
+          version = "23.05";
 
-  buildInputs = [ gmp libsodium sqlite zlib ];
+          src = fetchFromGitHub {
+            owner = "niftynei";
+            repo = "lightning";
+            rev = "44c5b523683160e8c20bda200c6a5a59ea40bc5e";
+            sha256 = "sha256-9T5d5W12v4b88FmCRiXhgQrzXOSCR5ByzT1ZJ05FI3A=";
+          };
 
-  # this causes some python trouble on a darwin host so we skip this step.
-  # also we have to tell libwally-core to use sed instead of gsed.
-  postPatch = if !stdenv.isDarwin then ''
-    patchShebangs \
-      tools/generate-wire.py \
-      tools/update-mocks.sh \
-      tools/mockup.sh \
-      devtools/sql-rewrite.py
-  '' else ''
-    substituteInPlace external/libwally-core/tools/autogen.sh --replace gsed sed && \
-    substituteInPlace external/libwally-core/configure.ac --replace gsed sed
-  '';
+          # when building on darwin we need dawin.cctools to provide the correct libtool
+          # as libwally-core detects the host as darwin and tries to add the -static
+          # option to libtool, also we have to add the modified gsed package.
+          nativeBuildInputs = with pkgs;
+            [autoconf autogen automake gettext libtool lowdown protobuf py3 unzip which]
+            ++ lib.optionals stdenv.isDarwin [darwin.cctools darwin.autoSignDarwinBinariesHook];
 
-  # configureFlags = [ "--enable-developer" ];
-  configureFlags = [ "--disable-developer" "--disable-valgrind" ];
+          buildInputs = [gmp libsodium sqlite zlib];
 
-  makeFlags = [ "VERSION=v${version}" ];
+          # this causes some python trouble on a darwin host so we skip this step.
+          # also we have to tell libwally-core to use sed instead of gsed.
+          postPatch =
+            if !stdenv.isDarwin
+            then ''
+              patchShebangs \
+                tools/generate-wire.py \
+                tools/update-mocks.sh \
+                tools/mockup.sh \
+                devtools/sql-rewrite.py
+            ''
+            else ''
+              substituteInPlace external/libwally-core/tools/autogen.sh --replace gsed sed && \
+              substituteInPlace external/libwally-core/configure.ac --replace gsed sed
+            '';
 
-  enableParallelBuilding = true;
+          configureFlags = ["--disable-developer" "--disable-valgrind"];
 
-  meta = with lib; {
-    description = "A Bitcoin Lightning Network implementation in C";
-    longDescription = ''
-      c-lightning is a standard compliant implementation of the Lightning
-      Network protocol. The Lightning Network is a scalability solution for
-      Bitcoin, enabling secure and instant transfer of funds between any two
-      parties for any amount.
-    '';
-    homepage = "https://github.com/ElementsProject/lightning";
-    maintainers = with maintainers; [ jb55 prusnak ];
-    license = licenses.mit;
-    platforms = platforms.linux ++ platforms.darwin;
-  };
+          makeFlags = ["VERSION=v${version}"];
+
+          enableParallelBuilding = true;
+
+          meta = with pkgs.lib; {
+            description = "A Bitcoin Lightning Network implementation in C";
+            longDescription = ''
+              c-lightning is a standard compliant implementation of the Lightning
+              Network protocol. The Lightning Network is a scalability solution for
+              Bitcoin, enabling secure and instant transfer of funds between any two
+              parties for any amount.
+            '';
+            homepage = "https://github.com/ElementsProject/lightning";
+            maintainers = with maintainers; [jb55 prusnak];
+            license = licenses.mit;
+            platforms = platforms.linux ++ platforms.darwin;
+          };
+        };
+    });
 }
-
